3c9c0426bae48bdb5798b8e40f06a061aa5d3da7
VegaDeg <99152395+VegaDeg@users.noreply.github.com>
2023-06-06 15:11:41 +0900

    Add files via upload

--- src/talker.cpp
+++ src/talker.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32.hpp"
 
 using namespace std::chrono_literals;
 
@@ -17,7 +17,7 @@ class MinimalPublisher : public rclcpp::Node
     MinimalPublisher()
     : Node("minimal_publisher"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<std_msgs::msg::Float32>("topic", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
@@ -25,14 +25,14 @@ class MinimalPublisher : public rclcpp::Node
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      auto message = std_msgs::msg::Float32();
+      message.data = count_++;
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", message.data);
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
+    float count_;
 };
 
 int main(int argc, char * argv[])
